0e37b9b2317780a825fdf82f02b5c344beb244ad
Anderson <5474@eclisboa.net>
2025-11-11 17:38:37 +0000

    lab1

---
+++ laboratorio1.java
@@ -0,0 +1,21 @@
+charset="UTF-8">
+    <title>A mminha primeira página</title>
+</head>   
+<body>
+    <h1>Programação na web</h1>
+    <p>Na <a href="http://escolacomerciolisboa.pt">Escola do Comércio de Lisboa</a>, na disciplina de Tecnologia 1 no 3ºBTIS, iremos aprender Programação web &#128517; 
+    e abordamos os principios básicos associados ao desenvolvimento para a Web, apenas do lado <i>front-end</i>, lado visual. Também sabmos que existe um 
+    lado <i>back-end</i>, que é o lado do servidor, mas nao iremos abordar esse lado.
+    </p>
+    
+    <p>Aprender-se-ão as seguintes tecnologias &#127760;:</p>
+    <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+    </ul>
+    <p>De forma a consolidar todos os conhecimentos, serão feitos alguns laboratórios.</p>
+
+    <img src="wordcloud.png">
+    </body>
+</html>
